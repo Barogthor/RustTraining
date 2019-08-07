ac7361828472c4f92630bcab58e399cecc6b002a
Barogthor <f.thery3103@gmail.com>
2019-08-07 11:40:29 +0200

    select sort: adding comparison closure

--- benches/sort_bench.rs
+++ benches/sort_bench.rs
@@ -2,7 +2,6 @@ extern crate rand;
 extern crate rust_training;
 extern crate bencher;
 
-
 use rand::Rng;
 use rust_training::sort::select_sort;
 use bencher::Bencher;
@@ -12,7 +11,7 @@ use bencher::benchmark_main;
 fn bench_select_sort(b: &mut Bencher) {
     let mut _rand = rand::thread_rng();
     let values: Vec<i64> = (0..1000).map(|_| _rand.gen_range(0, 1000)).collect();
-    b.iter(|| select_sort(&values));
+    b.iter(|| select_sort(&values, None));
 }
 
 benchmark_group!(benches, bench_select_sort);
