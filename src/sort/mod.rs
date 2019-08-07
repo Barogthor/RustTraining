10ac920154c86cf77a76a50edc353bf831aed666
Barogthor <f.thery3103@gmail.com>
2019-08-07 11:44:42 +0200

    select sort edit

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -14,7 +14,7 @@ pub fn select_sort<T>(array: &Vec<T>, cmp_closure: Option<Box<&Fn(&T,&T)->bool>>
     for i in 0..(sorted_array.len()-1){
         for j in (i+1)..sorted_array.len(){
             if cmp_ord(&sorted_array[j], &sorted_array[i]){
-                let tmp = sorted_array[i].clone();
+                let tmp = sorted_array[i].to_owned();
                 sorted_array[i] = sorted_array[j].to_owned();
                 sorted_array[j] = tmp;
             }
